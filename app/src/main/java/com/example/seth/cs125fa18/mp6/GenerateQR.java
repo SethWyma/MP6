047bf661cbb96e27b7ad1302d413afd37a5dbf75
SethWyma <wyma2@illinois.edu>
2018-11-29 00:31:43 -0600

    Created QR maker inputs

--- app/src/main/java/com/example/seth/cs125fa18/mp6/GenerateQR.java
+++ app/src/main/java/com/example/seth/cs125fa18/mp6/GenerateQR.java
@@ -1,7 +1,15 @@
 package com.example.seth.cs125fa18.mp6;
 
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 
 public class GenerateQR extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.generate_qr);
+    }
 }
